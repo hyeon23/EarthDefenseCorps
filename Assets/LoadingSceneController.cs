f5f36fa99b77c7b97f60ecd0367e1d175a76f8b0
hyeon23 <urim2270@naver.com>
2023-04-23 14:35:44 +0900

    Loading Scene Tips Update
    로딩씬 팁 업데이트 진행

--- Assets/LoadingSceneController.cs
+++ Assets/LoadingSceneController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Rendering;
 
 public class LoadingSceneController : MonoBehaviour
 {
@@ -14,8 +16,23 @@ public class LoadingSceneController : MonoBehaviour
     [SerializeField]
     Animator anime;
 
+    [SerializeField]
+    TextMeshProUGUI tipsTMP;
+
+    private string[] tipStrings;
+
     private void Start()
     {
+        tipStrings = new string[]{
+            "Tips. 이동은 좌우 슬라이드입니다.",
+            "Tips. 점프는 상 슬라이드입니다.",
+            "Tips. 쉴드는 하 슬라이드입니다.",
+            "Tips. 어택은 탭 입니다.",
+            "Tips. 쉴드를 통해 건물이나 적의 공격을 패링할 수 있습니다.",
+            "Tips. 필살기는 점프 상태에서 상 슬라이드입니다.",
+            "Tips. 쉴드와 필살기는 게이지가 있습니다.",
+        };
+
         StartCoroutine(LoadSceneProcess());
     }
 
@@ -32,7 +49,9 @@ public class LoadingSceneController : MonoBehaviour
 
         float timer = 0f;
 
-        while(!op.isDone) {
+        tipsTMP.text = tipStrings[Random.Range(0, tipStrings.Length)];
+
+        while (!op.isDone) {
 
             yield return null;
 
