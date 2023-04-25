ff03e538702dd3d02627aa91c96b7f543cb9b479
hyeon23 <urim2270@naver.com>
2023-04-25 14:43:25 +0900

    DB Init Update
    이따금씩 발생하는 뽑기시 흰 아이템이 뽑히는 현상[해결?]
    :아주 가끔씩 발생하긴 하는데 원인이 안잡힘
    예상원인1: DataManager 클릭 시, 장비에 null값이 들어가 오류로 인해 발생
    예상원인2: DataManager 아이템 DB 초기화 시기에 따른 에러(O)
    아이템 DB가 정상적으로 초기화 되지 않은 상황에서 뽑으니 발생하는 에러였음
    =DataManager의 초기화순서를 앞으로 설정해서 해결

--- Assets/Scripts/EquipSlot.cs
+++ Assets/Scripts/EquipSlot.cs
@@ -11,171 +11,70 @@ public class EquipSlot : MonoBehaviour
     public EquipSlotPart equipSlotPart;
     public TextMeshProUGUI curLevelText;
 
-    public Item item = null;
+    public Item item;
 
     private void Update()
     {
-        if (item != null || item.itemPart != ItemPart.None)
-        {
-            curLevelText.gameObject.SetActive(true);
-        }
-        else
-        {
-            curLevelText.gameObject.SetActive(false);
-        }
-
         switch (equipSlotPart)
         {
             case EquipSlotPart.Weapon:
                 item = DataManager.Instance.playerData.CurEquippedWeapon;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
                 break;
             case EquipSlotPart.Gloves:
                 item = DataManager.Instance.playerData.CurEquippedGloves;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
                 break;
             case EquipSlotPart.Shoes:
                 item = DataManager.Instance.playerData.CurEquippedShoes;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    item = null;
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
                 break;
             case EquipSlotPart.Sheld:
                 item = DataManager.Instance.playerData.CurEquippedSheld;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    item = null;
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
                 break;
             case EquipSlotPart.Helmat:
                 item = DataManager.Instance.playerData.CurEquippedHelmat;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    item = null;
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
                 break;
             case EquipSlotPart.Armor:
                 item = DataManager.Instance.playerData.CurEquippedArmor;
-
-                if (item != null || item.itemPart != ItemPart.None)
-                {
-                    gameObject.GetComponent<Button>().interactable = true;
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 1;
-                    ItemIcon.color = tempColor;
-                    ItemIcon.sprite = item.itemImage;
-                    curLevelText.text = $"Lv.{item.itemCurLevel}";
-                    gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(item);
-                }
-                else
-                {
-                    item = null;
-                    curLevelText.text = "";
-                    Color tempColor = ItemIcon.color;
-                    tempColor.a = 0;
-                    ItemIcon.color = tempColor;
-                    gradeBackground.color = new Color(1, 1, 1, 1);
-                    gameObject.GetComponent<Button>().interactable = false;
-                    gradeBackground.color = Color.white;
-                }
+                break;
+            default:
+                item = null;
                 break;
         }
+
+        EquippedItemPresentation(item);
     }
 
     public void ItemSlotClicked()
     {
         SoundManager.Instance.SFXPlay(SoundManager.SFX.Button);
         MainMenuController.Instance.EquipInfoUpdate(item);
+    }
+
+    public void EquippedItemPresentation(Item _item)
+    {
+        if (_item != null)
+        {
+            gameObject.GetComponent<Button>().interactable = true;
+            Color tempColor = ItemIcon.color;
+            tempColor.a = 1;
+
+            ItemIcon.color = tempColor;
+            ItemIcon.sprite = _item.itemImage;
+
+            curLevelText.gameObject.SetActive(true);
+            curLevelText.text = $"Lv.{_item.itemCurLevel}";
+            gradeBackground.color = MainMenuController.Instance.SetGradeColorBackground(_item);
+        }
+        else
+        {
+            Color tempColor = ItemIcon.color;
+            tempColor.a = 0;
+            ItemIcon.color = tempColor;
+
+            curLevelText.gameObject.SetActive(false);
+
+            gameObject.GetComponent<Button>().interactable = false;
+
+            gradeBackground.color = Color.white;
+        }
     }
 }
